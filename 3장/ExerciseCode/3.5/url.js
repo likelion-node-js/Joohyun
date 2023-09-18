686012e3b5e8ed3b3c6629e0e92eeb96b5c57555
reinexxism <lucy0805@naver.com>
2023-09-18 14:43:37 +0900

    0918 url module

---
+++ 3장/ExerciseCode/3.5/url.js
@@ -0,0 +1,9 @@
+// url 모듈을 사용하기 위해 불러오기
+const url = require("url");
+
+const { URL } = url;
+const myURL = new URL(
+  "http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor"
+);
+console.log("new URL():", myURL);
+console.log("url.format():", url.format(myURL));
